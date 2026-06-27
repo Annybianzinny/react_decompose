bedfcf357df422467a7b4681e5b06e0cf48a2592
Anik <anikferreira8@gmail.com>
2026-06-27 15:51:32 -0300

    React decompose

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,5 +1,29 @@
-// import a css file containig article styles
+import "./Article.css";
 
-// Create an Article function returning the HTML of article block
+export default function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Headline</h2>
 
-// Add a default export statement for Article component to use it in the other files
+      <p className="article__paragraph">
+        In elementum lorem eget est euismod ornare...
+      </p>
+
+      <p className="article__paragraph">
+        In ac nisi lacus. Fusce est dolor...
+      </p>
+
+      <p className="article__paragraph">
+        Donec arcu elit, euismod vel lobortis...
+      </p>
+
+      <p className="article__paragraph">
+        Cras egestas tempor nibh...
+      </p>
+
+      <p className="article__paragraph">
+        Pellentesque habitant morbi...
+      </p>
+    </article>
+  );
+}
